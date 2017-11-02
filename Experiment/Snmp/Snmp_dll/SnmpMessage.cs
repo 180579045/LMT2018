521d7546e2efb26e9a7f301be83b53b1d217ca67
Guoliang3 <msn103125@126.com>
2017-11-02 18:28:33 +0800

    update snmp dll project

---
+++ Experiment/Snmp/Snmp_dll/SnmpMessage.cs
@@ -0,0 +1,43 @@
+﻿/*----------------------------------------------------------------
+// Copyright (C) 2017 大唐移动通信设备有限公司 版权所有;
+//
+// 文件名：SnmpMessage.cs
+// 文件功能描述：Snmp报文类;
+// 创建人：郭亮;
+// 版本：V1.0
+// 创建标识：创建文件;
+// 时间：2017-11-2
+//----------------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snmp_dll
+{
+    public delegate void SnmpType();
+
+    public class SnmpMessage
+    {
+        private string m_IPAddr;
+        private string m_Community;
+        private List<string> m_PduList;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void GetRequest(List<string> PduList, string Community, SnmpType SnmpType)
+        {
+
+        }
+
+        public void Type_GetRequest()
+        {
+
+        }
+
+
+    }
+}
