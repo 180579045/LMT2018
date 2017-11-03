984451377c2e264ca7043520c11f3967aa89a282
Guoliang3 <msn103125@126.com>
2017-11-03 14:37:19 +0800

    try proj

--- Experiment/Snmp/Snmp_dll/SnmpMessage.cs
+++ Experiment/Snmp/Snmp_dll/SnmpMessage.cs
@@ -17,7 +17,7 @@ using System.Threading.Tasks;
 
 namespace Snmp_dll
 {
-    public delegate void SnmpType();
+    public delegate void SnmpType(List<string> PduList, string Community, string IpAddress);
 
     public class SnmpMessage
     {
@@ -26,16 +26,65 @@ namespace Snmp_dll
         private List<string> m_PduList;
 
         /// <summary>
-        /// 
+        /// GetRequest的对外接口，前三个参数为Snmp报文所必须的，最后一个是GetRequest的模式
         /// </summary>
-        public void GetRequest(List<string> PduList, string Community, SnmpType SnmpType)
+        public void GetRequest(List<string> PduList, string Community, string IpAddress, SnmpType SnmpType)
         {
-            return;
+            SnmpType(PduList, Community, IpAddress);
         }
 
-        public void Type_GetRequest()
+        public static void Type_GetRequest_V2c(List<string> PduList, string Community, string IpAddress)
         {
+            // SNMP community name
+            OctetString community = new OctetString("public");
 
+            // Define agent parameters class
+            AgentParameters param = new AgentParameters(community);
+            // Set SNMP version to 1 (or 2)
+            param.Version = SnmpVersion.Ver2;
+            // Construct the agent address object
+            // IpAddress class is easy to use here because
+            //  it will try to resolve constructor parameter if it doesn't
+            //  parse to an IP address
+            IpAddress agent = new IpAddress("172.27.245.92");
+
+            // Construct target
+            UdpTarget target = new UdpTarget((IPAddress)agent, 161, 2000, 1);
+
+            // Pdu class used for all requests
+            Pdu pdu = new Pdu(PduType.Get);
+            pdu.VbList.Add("1.3.6.1.4.1.5105.100.1.9.4.7.0");
+
+            // Make SNMP request
+            SnmpV2Packet result = (SnmpV2Packet)target.Request(pdu, param);
+
+            // If result is null then agent didn't reply or we couldn't parse the reply.
+            if (result != null)
+            {
+                // ErrorStatus other then 0 is an error returned by 
+                // the Agent - see SnmpConstants for error definitions
+                if (result.Pdu.ErrorStatus != 0)
+                {
+                    // agent reported an error with the request
+                    Console.WriteLine("Error in SNMP reply. Error {0} index {1}",
+                        result.Pdu.ErrorStatus,
+                        result.Pdu.ErrorIndex);
+                }
+                else
+                {
+                    // Reply variables are returned in the same order as they were added
+                    //  to the VbList
+                    Console.WriteLine("sysDescr({0}) ({1}): {2}",
+                        result.Pdu.VbList[0].Oid.ToString(),
+                        SnmpConstants.GetTypeName(result.Pdu.VbList[0].Value.Type),
+                        result.Pdu.VbList[0].Value.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("No response received from SNMP agent.");
+            }
+            target.Close();
         }
 
 
