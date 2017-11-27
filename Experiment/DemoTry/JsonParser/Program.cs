99570d712a6a98ff32f2b698b7727a40bb5f3bbf
Guoliang3 <msn103125@126.com>
2017-11-27 13:28:33 +0800

    rename project

--- Experiment/DemoTry/JsonParser/Program.cs
+++ Experiment/DemoTry/JsonParser/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace JsonParser
 {
@@ -10,6 +13,15 @@ namespace JsonParser
     {
         static void Main(string[] args)
         {
+            LoadJsonFile();
+        }
+
+        static void LoadJsonFile()
+        {
+            using (StreamReader reader = File.OpenText(@"Data\Tree_Reference.json"))
+            {
+                JObject o = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+            }
         }
     }
 }
