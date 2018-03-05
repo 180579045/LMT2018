7934999e35f38937526c57a1f72b9121cda30a41
wangxiaoying1 <wangxiaoying1@datangmobile.cn>
2018-03-05 11:29:15 +0800

    增加轮循收藏节点的方法,修改增加treenode的过滤

--- Src/SCMT/MainWindow/Component/View/ObjTreeNode.cs
+++ Src/SCMT/MainWindow/Component/View/ObjTreeNode.cs
@@ -16,6 +16,7 @@ using System.Threading.Tasks;
 using UICore.Controls.Metro;
 using System.Windows.Controls;
 using System.Windows;
+using System.Windows.Input;
 
 namespace SCMTMainWindow
 {
@@ -42,6 +43,7 @@ namespace SCMTMainWindow
 
         public event EventHandler IsExpandedChanged;             // 树形结构展开时;
         public event EventHandler IsSelectedChanged;             // 树形结构节点被选择时;
+        public event MouseButtonEventHandler IsRightMouseDown;   // 右键选择节点时;
 
         /// <summary>
         /// 对象树节点点击事件;
@@ -97,6 +99,7 @@ namespace SCMTMainWindow
                     item.SubExpender = Lists;                           // 增加子容器,保存叶子节点;
                     item.obj_type = Obj_Node;                           // 将节点添加到容器控件中;
                     item.Click += IsSelectedChanged;                    // 点击事件;
+                    item.RightClick += IsRightMouseDown;
 
                     Obj_Tree.Add(item);
 
@@ -106,7 +109,79 @@ namespace SCMTMainWindow
                 
             }
         }
+        public void TraverseCollectChildren(MetroExpander Obj_Tree, StackPanel Lists, int depths, Grid ContentGrid, MetroScrollViewer NB_Base_Container)
+        {
+            Thickness margin = new Thickness();
+            // 遍历所有对象树节点;
+            foreach (var Obj_Node in SubObj_Lsit)
+            {
+                Obj_Node.m_NB_Base_Contain = NB_Base_Container;
+                Obj_Node.m_NB_ContentGrid = ContentGrid;
+                // 只有枝节点才能够加入对象树;
+                if (Obj_Node is ObjTreeNode)
+                {
+                    // 新建一个对象树节点容器控件;
+                    MetroExpander item = new MetroExpander();
 
+                    // 判断孩子中是否还包含枝节点;
+                    bool NotContainTree = true;
+                    if (Obj_Node.SubObj_Lsit != null)
+                    {
+                        foreach (var isTree in Obj_Node.SubObj_Lsit)
+                        {
+                            if (isTree is ObjTreeNode)
+                            {
+                                NotContainTree = false;
+                            }
+                        }
+                    }
+                    // 将孩子没有枝节点的节点进行缩进处理;
+                    if (NotContainTree)
+                    {
+                        margin.Left = 35 + depths;
+                        margin.Top = 8;
+                        margin.Bottom = 8;
+                        item.Margin = margin;
+                    }
+                    else
+                    {
+                        margin.Left = 20 + depths;
+                        margin.Top = 8;
+                        margin.Bottom = 8;
+                        item.ARMargin = margin;
+                    }
+
+                    item.Header = Obj_Node.ObjName;
+                    item.SubExpender = Lists;                           // 增加子容器,保存叶子节点;
+                    item.obj_type = Obj_Node;                           // 将节点添加到容器控件中;
+                    item.Click += IsSelectedChanged;                    // 点击事件;
+                    //item.MouseRightButtonDown += IsRightMouseDown;
+
+                    Obj_Tree.Add(item);
+                    // 递归孩子节点;
+                    Obj_Node.TraverseChildren(item, Lists, depths + 15, ContentGrid, NB_Base_Container);
+                }
+                else
+                {
+                    // 新建一个对象树节点容器控件;
+                    MetroExpander item = new MetroExpander();
+
+                    margin.Left = 35 + depths;
+                    margin.Top = 8;
+                    margin.Bottom = 8;
+                    item.Margin = margin;
+
+                    item.Header = Obj_Node.ObjName;
+                    item.SubExpender = Lists;                           // 增加子容器,保存叶子节点;
+                    item.obj_type = Obj_Node;                           // 将节点添加到容器控件中;
+                    item.Click += IsSelectedChanged;                    // 点击事件;
+                    //item.MouseRightButtonDown += IsRightMouseDown;
+
+                    Obj_Tree.Add(item);
+                }
+
+            }
+        }
         /// <summary>
         /// 初始化参数列表;
         /// </summary>
@@ -118,7 +193,27 @@ namespace SCMTMainWindow
             this.ObjName = name;
             IsSelectedChanged += ClickObjNode;
         }
-        
+
+        private void ObjNode_IsRightMouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+            MetroExpander abc = e.Source as MetroExpander;
+            Console.WriteLine("111" + (abc.obj_type as ObjNode).ObjName);
+            MainWindow.StrNodeName = (abc.obj_type as ObjNode).ObjName;
+            //menu.Show(control, new Point(e.X, e.Y));   //在点(e.X, e.Y)处显示menu
+
+
+            //MetroExpander abc = e.Source as MetroExpander;
+            //Console.WriteLine("111" + (abc.obj_type as ObjNode).ObjName);
+            ////this.ObjName = (abc.obj_type as ObjNode).ObjName;
+            //MenuItem[] formMenuItemList = new MenuItem[1];
+            ////formMenuItemList[0] = new MenuItem("添加到收藏", null, new EventHandler(AddToCollect(sender, e)));
+            //formMenuItemList[0] = new MenuItem();
+            //ContextMenu formMenu = new ContextMenu();
+            //formMenu.Items.Add(formMenuItemList);
+            //this.ContextMenu = formMenu;
+        }
+
     }
     /// <summary>
     /// 对象树*普通树枝*节点;
@@ -146,22 +241,36 @@ namespace SCMTMainWindow
             items.SubExpender.Children.Clear();
 
             // 将叶子节点加入右侧容器;
-            foreach (var iter in (items.obj_type as ObjNode).SubObj_Lsit )
+            if ((items.obj_type as ObjNode).SubObj_Lsit != null)
             {
-                // 子节点如果是枝节点跳过;
-                if (iter is ObjTreeNode)
+                foreach (var iter in (items.obj_type as ObjNode).SubObj_Lsit)
                 {
-                    continue;
-                }
+                    // 子节点如果是枝节点跳过;
+                    if (iter is ObjTreeNode)
+                    {
+                        continue;
+                    }
 
-                // 初始化对应的内容,并加入到容器中;
-                MetroExpander subitems = new MetroExpander();
-                subitems.Header = iter.ObjName;
-                subitems.Click += iter.ClickObjNode;
-                subitems.obj_type = iter;
-                items.SubExpender.Children.Add(subitems);
+                    // 初始化对应的内容,并加入到容器中;
+                    MetroExpander subitems = new MetroExpander();
+                    subitems.Header = iter.ObjName;
+                    subitems.Click += iter.ClickObjNode;
+                    subitems.obj_type = iter;
+                    items.SubExpender.Children.Add(subitems);
+                }
             }
-            
+            else
+            {
+                MetroExpander item = sender as MetroExpander;
+                ObjNode node = item.obj_type as ObjNode;
+
+                Console.WriteLine("LeafNode Clicked!" + node.ObjName);
+                item.NBBase_Grid = node.m_NB_Base_Contain;
+                item.NBContent_Grid = node.m_NB_ContentGrid;
+                item.NBBase_Grid.Visibility = Visibility.Hidden;
+                item.NBContent_Grid.Visibility = Visibility.Visible;
+            }
+
         }
 
         /// <summary>
@@ -170,7 +279,17 @@ namespace SCMTMainWindow
         /// <param name="obj">孩子节点</param>
         public override void Add(ObjNode obj)
         {
-            SubObj_Lsit.Add(obj);
+            int index = SubObj_Lsit.IndexOf(obj);
+            if (index < 0)
+            {
+                SubObj_Lsit.Add(obj);
+                Console.WriteLine("222" + obj.ObjName);
+            }
+            else
+            {
+                Console.WriteLine("添加重复节点");
+            }
+            //SubObj_Lsit.Add(obj);
         }
 
         /// <summary>
