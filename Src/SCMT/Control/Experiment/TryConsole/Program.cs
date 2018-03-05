8133b1a92316281073aab27578cb6acddc4f7d06
Guoliang3 <guoliang88925@gmail.com>
2018-03-05 14:21:22 +0800

    update

--- Src/SCMT/Control/Experiment/TryConsole/Program.cs
+++ Src/SCMT/Control/Experiment/TryConsole/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using SCMTOperationCore;
+using System.Collections.Generic;
 using System.Net;
 using SCMTOperationCore.Elements;
+using SCMTOperationCore.Message;
 
 namespace TryConsole
 {
@@ -11,12 +12,12 @@ namespace TryConsole
         {
             // 客户端可以实例化一个si实例;
             NodeB nb = new NodeB();
+            List<string> oids = new List<string>();
             nb.m_IPAddress = IPAddress.Parse("172.27.245.92"); 
             nb.Connect();
-
             
-
-            Console.WriteLine("Connecting");
+            Console.WriteLine("Connected！");
+            
             Console.ReadLine();
         }
     }
