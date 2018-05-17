c92188eba10f58f1cbaafe9930782059e8749fc9
wangxiaoying1 <wangxiaoying1@datangmobile.cn>
2018-05-17 17:21:42 +0800

    添加系统日志记录

---
+++ Src/SCMT/Control/LogManager/Log.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "Log4.config", Watch = true)]
+namespace LogManager
+{
+    public class Log
+    {
+        /// <summary>
+        /// 输出日志到Log4Net
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="ex"></param>
+        #region static void WriteLog(Type t, Exception ex)
+
+        //错误信息
+        public static void WriteLogError(Type t, Exception ex)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(t);
+            log.Error("Error", ex);
+        }
+
+        #endregion
+        //严重错误
+        public static void WriteLogFatal(Type t, Exception ex)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(t);
+            log.Fatal("Fatal", ex);
+        }
+
+        /// <summary>
+        /// 输出日志到Log4Net
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="msg"></param>
+        #region static void WriteLog(Type t, string msg)
+
+        //一般信息
+        public static void WriteLogInfo(Type t, string msg)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(t);
+            log.Info(msg);
+        }
+
+        #endregion
+
+        public static void WriteLogDebug(Type t, string msg)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(t);
+            log.Debug(msg);
+        }
+
+        public static void WriteLogWarn(Type t, string msg)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(t);
+            log.Warn(msg);
+        }
+    }
+}
