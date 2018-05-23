bec1c719d67e4c310c43138db2cbc1a651676a98
冯严峰 <fengyanfeng@datangmobile.cn>
2018-05-23 09:33:58 +0800

    补充snmp命令功能

---
+++ Src/SCMT/Control/SCMTOperationCore/Message/SNMP/CmdExecuteMgr.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCMTOperationCore.Message.SNMP
+{
+    /// <summary>
+    /// 以命令名称方式发送Snmp消息
+    /// </summary>
+    public sealed class CmdExecuteMgr
+    {
+        private static CmdExecuteMgr _instance = null;
+
+        private static readonly object SynObj = new object();
+
+        private CmdExecuteMgr()
+        {
+        }
+
+        public static CmdExecuteMgr GetInstance()
+        {
+            if (null == _instance)
+            {
+                lock(SynObj)
+                {
+                    if (null == _instance)
+                    {
+                        _instance = new CmdExecuteMgr();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+
+        /// <summary>
+        /// 执行一条类型为Get的同步操作命令
+        /// </summary>
+        /// <param name="cmdName"></param>
+        /// <param name="requestId"></param>
+        /// <param name="strIndex"></param>
+        /// <param name="strIpAddr"></param>
+        /// <param name="lmtPdu"></param>
+        /// <returns></returns>
+        public int CmdGetSync(string cmdName, out long requestId, string strIndex
+                              , string strIpAddr, ref LmtPdu lmtPdu)
+        {
+            requestId = 0;
+            if (string.IsNullOrEmpty(cmdName) || string.IsNullOrEmpty(strIpAddr))
+            {
+                return -1;
+            }
+
+            // TODO: 数据库先打桩
+            string strMibList = "";
+
+            if (string.IsNullOrEmpty(strMibList))
+            {
+                return -1;
+            }
+
+            // 转换为oid数组
+            string[] mibList = StringToArray(strMibList);
+            if (mibList == null)
+            {
+                return -1;
+            }
+
+
+            // TODO : bNeedCheck
+
+            // TODO:
+            // 获取oid的前缀
+            string strPreFixOid = "";
+            string strOid;
+
+            foreach(string v in mibList)
+            {
+                //strOid.
+            }
+
+            return 0;
+        }
+
+        private string[] StringToArray(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return null;
+            }
+
+            return str.Split('|');
+        }
+        
+    }
+}
---
+++ Src/SCMT/SCMTOperationCoreTests/Message/SNMP/CmdExecuteMgrTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SCMTOperationCore.Message.SNMP;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCMTOperationCore.Message.SNMP.Tests
+{
+    [TestClass()]
+    public class CmdExecuteMgrTests
+    {
+        [TestMethod()]
+        public void GetInstanceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CmdGetSyncTest()
+        {
+            Debug.WriteLine("kkk");
+
+
+            string cmdName = "aaa";
+            long requestId = 0;
+            string strIndex = ".0";
+            string strIpAddr = "192.168.5.198";
+            LmtPdu lmtPdu = new LmtPdu2c();
+
+            CmdExecuteMgr.GetInstance().CmdGetSync(cmdName, out requestId, strIndex
+                , strIpAddr, ref lmtPdu);
+
+ //           Assert.Fail();
+        }
+    }
+}
