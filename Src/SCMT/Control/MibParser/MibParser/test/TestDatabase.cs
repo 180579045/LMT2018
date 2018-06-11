bf27c44aee5fa322ef28b96d9bec550a49f77da0
luanyibo <luanyibo_sky@163.com>
2018-06-11 16:10:30 +0800

    -2

---
+++ Src/SCMT/Control/MibParser/MibParser/test/TestDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MIBDataParser.JSONDataMgr;
+
+namespace MIBDataParser.test
+{
+    class TestDatabase
+    {
+        public void test1()
+        {
+            Database dbTest = Database.GetInstance();
+
+
+        }
+    }
+}
