fae2a5fe2a1b2f624428b295903888e2b2b2aebe
houshangling <houshangling@datangmobile.cn>
2018-06-11 19:09:06 +0800

    增加文件管理模块Cfg配置文件的校验功能

---
+++ Src/SCMT/FileManager/FileHandler/CfgFileSelDlg.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace FileManager.FileHandler
+{
+	partial class CfgFileSelDlg
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.IDOK = new System.Windows.Forms.Button();
+			this.IDCANCEL = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "配置文件",
+            "一般文件",
+            "RNC容灾数据文件"});
+			this.comboBox1.Location = new System.Drawing.Point(42, 53);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(289, 20);
+			this.comboBox1.TabIndex = 0;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// IDOK
+			// 
+			this.IDOK.Location = new System.Drawing.Point(79, 140);
+			this.IDOK.Name = "IDOK";
+			this.IDOK.Size = new System.Drawing.Size(75, 23);
+			this.IDOK.TabIndex = 1;
+			this.IDOK.Text = "确定";
+			this.IDOK.UseVisualStyleBackColor = true;
+			this.IDOK.Click += new System.EventHandler(this.IDOK_Click);
+			// 
+			// IDCANCEL
+			// 
+			this.IDCANCEL.Location = new System.Drawing.Point(202, 140);
+			this.IDCANCEL.Name = "IDCANCEL";
+			this.IDCANCEL.Size = new System.Drawing.Size(75, 23);
+			this.IDCANCEL.TabIndex = 2;
+			this.IDCANCEL.Text = "取消";
+			this.IDCANCEL.UseVisualStyleBackColor = true;
+			this.IDCANCEL.Click += new System.EventHandler(this.IDCANCEL_Click);
+			// 
+			// CfgFileSelDlg
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(367, 212);
+			this.ControlBox = false;
+			this.Controls.Add(this.IDCANCEL);
+			this.Controls.Add(this.IDOK);
+			this.Controls.Add(this.comboBox1);
+			this.Name = "CfgFileSelDlg";
+			this.ShowIcon = false;
+			this.Text = "请选择文件下载类型";
+			this.Load += new System.EventHandler(this.form_load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button IDOK;
+		private System.Windows.Forms.Button IDCANCEL;
+	}
+}
