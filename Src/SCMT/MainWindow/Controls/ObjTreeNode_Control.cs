c20d0b2db4abd8bfedca9d488fbf9a019373f961
houshangling <houshangling@datangmobile.cn>
2018-06-20 15:19:34 +0800

    主界面解析对象树流程修正：连接基站成功后初始化对象树。 目前版本还存在其他的BUG需要处理

--- Src/SCMT/MainWindow/Controls/ObjTreeNode_Control.cs
+++ Src/SCMT/MainWindow/Controls/ObjTreeNode_Control.cs
@@ -13,117 +13,139 @@ using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.IO;
+using System.Text;
 using SCMTOperationCore.Elements;
 using System.Threading.Tasks;
 using System.Windows;
+using CommonUtility;
 
 namespace SCMTMainWindow
 {
-    /// <summary>
-    /// 对象树管理类;
-    /// </summary>
-    class ObjNodeControl
-    {
-        public NodeB m_NodeB { get; set; }                          // 对应的基站;
-        public List<ObjNode> m_NodeList { get; set; }               // 对用的对象树列表(后续会改为以基站为索引的dictionary);
-        public List<ObjNode> m_RootNode { get; set; }               // Demo只有一个基站，暂时用来保存根节点;
-        private string m_ObjFilePath;                               // 暂时用一下，保存JSON文件路径;
+	/// <summary>
+	/// 对象树管理类;
+	/// </summary>
+	class ObjNodeControl
+	{
+		public NodeB m_NodeB { get; set; }                          // 对应的基站;
+		public List<ObjNode> m_NodeList { get; set; }               // 对用的对象树列表(后续会改为以基站为索引的dictionary);
+		public List<ObjNode> m_RootNode { get; set; }               // Demo只有一个基站，暂时用来保存根节点;
+		private string m_ObjFilePath;                               // 暂时用一下，保存JSON文件路径;
 
-        /// <summary>
-        /// 实验程序先假定一个LMT只连接一个基站，在构造函数中直接读取默认JSON文件;
-        /// </summary>
-        public ObjNodeControl(NodeB node)
-        {
-            m_ObjFilePath = node.m_ObjTreeDataPath;
-            JObject JObj = new JObject();
-            try
-            {
-                using (StreamReader reader = File.OpenText(m_ObjFilePath))
-                {
-                    JObj = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
-                }
+		/// <summary>
+		/// 实验程序先假定一个LMT只连接一个基站，在构造函数中直接读取默认JSON文件;
+		/// </summary>
+		public ObjNodeControl(NodeB node)
+		{
+			m_ObjFilePath = node.m_ObjTreeDataPath;
+			//JObject JObj = new JObject();
+			try
+			{
+				//using (StreamReader reader = File.OpenText(m_ObjFilePath))
+				//{
+				//	JObj = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+				//}
 
-                ObjNode.nodeb = node;
-                ParseJObject(JObj);
-            }
-            catch(Exception e)
-            {
-                MessageBox.Show("1加载数据库失败;" + e.ToString());
-            }
-        }
+				ObjNode.nodeb = node;
 
-        /// <summary>
-        /// 解析JObject到一个容器当中;
-        /// </summary>
-        /// <param name="obj">从文件中解析出来的JSON对象;</param>
-        /// <returns>返回一个保存了所有节点的容器;</returns>
-        private void ParseJObject(JObject obj)
-        {
-            // 筛选出所有节点的ID;
-            IEnumerable<int> AllNodes = from nodes in obj.First.Next.First select (int)nodes["ObjID"];
-            m_NodeList = new List<ObjNode>();
-            int TempCount = 0;
+				var jsonContent = File.ReadAllText(m_ObjFilePath, Encoding.Default);
+				var nodeList = JsonHelper.SerializeJsonToObject<Nodes>(jsonContent);
 
-            
-            // 遍历所有JSON文件中的节点,并添加进列表中;
-            foreach (var iter in AllNodes)
-            {
-                ObjNode node;
-                var ObjParentNodes = (int)obj.First.Next.First[TempCount]["ObjParentID"];
-                var name = (string)obj.First.Next.First[TempCount]["ObjName"];
-                var TableName = (string)obj.First.Next.First[TempCount]["MibTableName"];
-                var version = (string)obj.First.First;
+				ParseJObject(nodeList);
+			}
+			catch(Exception e)
+			{
+				MessageBox.Show("1加载数据库失败;" + e.ToString());
+			}
+		}
 
-                if ((int)obj.First.Next.First[TempCount]["ChildRenCount"] != 0)
-                {
-                    node = new ObjTreeNode(iter, ObjParentNodes, version, name, TableName);
-                }
-                else
-                {
-                    node = new ObjLeafNode(iter, ObjParentNodes, version, name, TableName);
-                }
+		/// <summary>
+		/// 解析JObject到一个容器当中;
+		/// </summary>
+		/// <param name="obj">从文件中解析出来的JSON对象;</param>
+		/// <returns>返回一个保存了所有节点的容器;</returns>
+		private void ParseJObject(Nodes nodes)
+		{
+			var AllNodes = nodes.NodeList;
+			m_NodeList = new List<ObjNode>();
+			int TempCount = 0;
 
-                m_NodeList.Add(node);
-                TempCount++;
-            }
-            m_RootNode = ArrangeParentage(m_NodeList);
+			var version = nodes.version;
 
-            
-            
-        }
+			// 遍历所有JSON文件中的节点,并添加进列表中;
+			foreach (var iter in AllNodes)
+			{
+				var ObjParentNodes = iter.ObjParentID;
+				var name = iter.ObjName;
+				var TableName = iter.MibTableName;
 
-        /// <summary>
-        /// 确认亲子关系;
-        /// </summary>
-        /// <param name="NodeList"></param>
-        /// <returns></returns>
-        public static List<ObjNode> ArrangeParentage(List<ObjNode> NodeList)
-        {
-            List<ObjNode> RootNodeShow = new List<ObjNode>();
-            ObjNode Root = new ObjTreeNode(0, 0, "1.0", "基站节点列表",@"/");
+				ObjNode objNode;
+				if (iter.ChildRenCount != 0)
+				{
+					objNode = new ObjTreeNode(iter.ObjID, ObjParentNodes, version, name, TableName);
+				}
+				else
+				{
+					objNode = new ObjLeafNode(iter.ObjID, ObjParentNodes, version, name, TableName);
+				}
 
-            // 遍历所有节点确认亲子关系;
-            foreach (ObjNode iter in NodeList)
-            {
-                if (iter.ObjParentID == 0)
-                {
-                    Root.Add(iter);
-                }
-                else
-                {
-                    foreach (ObjNode iterParent in NodeList)
-                    {
-                        if (iterParent.ObjID == iter.ObjParentID)
-                        {
-                            iterParent.Add(iter);
-                        }
-                    }
-                }
-            }
+				m_NodeList.Add(objNode);
+			}
+			m_RootNode = ArrangeParentage(m_NodeList);
+		}
 
-            RootNodeShow.Add(Root);
+		/// <summary>
+		/// 确认亲子关系;
+		/// </summary>
+		/// <param name="NodeList"></param>
+		/// <returns></returns>
+		public static List<ObjNode> ArrangeParentage(List<ObjNode> NodeList)
+		{
+			List<ObjNode> RootNodeShow = new List<ObjNode>();
+			ObjNode Root = new ObjTreeNode(0, 0, "1.0", "基站节点列表",@"/");
 
-            return RootNodeShow;
-        }
-    }
+			// 遍历所有节点确认亲子关系;
+			foreach (ObjNode iter in NodeList)
+			{
+				if (iter.ObjParentID == 0)
+				{
+					Root.Add(iter);
+				}
+				else
+				{
+					foreach (ObjNode iterParent in NodeList)
+					{
+						if (iterParent.ObjID == iter.ObjParentID)
+						{
+							iterParent.Add(iter);
+						}
+					}
+				}
+			}
+
+			RootNodeShow.Add(Root);
+
+			return RootNodeShow;
+		}
+	}
+
+	#region 解析json文件所需的类
+
+	internal class OneNode
+	{
+		public int ObjID;
+		public int ObjParentID;
+		public int ChildRenCount;
+		public string ObjName;
+		public string ObjNameEn;
+		public string MibTableName;
+		public string MIBList;
+	}
+
+	internal class Nodes
+	{
+		public string version;
+		public List<OneNode> NodeList;
+	}
+
+	#endregion
 }
