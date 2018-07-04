5904a6b7e8557262f845a3d071c76a42bb0d6f16
houshangling <houshangling@datangmobile.cn>
2018-07-04 14:28:19 +0800

    增加单元测试用例

---
+++ Src/SCMT/Control/SCMTOperationCoreTests/Message/MsgDispatcher/MsgDispatcherTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SCMTOperationCore.Message.MsgDispatcher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MsgQueue;
+
+namespace SCMTOperationCore.Message.MsgDispatcher.Tests
+{
+	[TestClass()]
+	public class MsgDispatcherTests
+	{
+		[TestMethod()]
+		public void OnEnbPhaseMsgTest()
+		{
+			var data = new byte[80];
+			var msg = new SubscribeMsg(data, "test");
+			
+		}
+	}
+}
