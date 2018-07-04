8044e621d55f08d6260011ac910727c14388e02a
houshangling <houshangling@datangmobile.cn>
2018-07-04 14:27:01 +0800

    消息队列增加两个助手：前台显示日志信息助手；Topic定义助手，避免错误

---
+++ Src/SCMT/MsgQueue/TopicHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 保存所有的topic，类似MFC中的消息定义
+
+namespace MsgQueue
+{
+	public class TopicHelper
+	{
+		#region B方案中使用的topic
+
+
+		#endregion
+
+		#region 通知UI界面相关消息
+		public const string SHOW_LOG = "show_log";
+
+
+		#endregion
+
+		#region 后台进程间消息
+
+		public const string EnbConnectedMsg = "handle_enb_connected";
+
+		#endregion
+
+		#region SI消息对应的topic
+		public const string EnbPhaseMsg = "handle_enbphase_msg";
+		public const string QuerySiPortVerRsp = "handle_si_port_ver_rsp";
+		#endregion
+
+	}
+}
