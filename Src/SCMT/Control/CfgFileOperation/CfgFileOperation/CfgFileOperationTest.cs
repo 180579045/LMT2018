f701821cee350b76487fcbea784f1d992619785e
luanyibo <luanyibo_sky@163.com>
2018-08-08 17:34:58 +0800

    cfg 操作的架子

---
+++ Src/SCMT/Control/CfgFileOperation/CfgFileOperation/CfgFileOperationTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CfgFileOperation
+{
+    /// <summary>
+    /// .cfg 文件相关操作的测试代码
+    /// </summary>
+    class CfgFileOperationTest
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
