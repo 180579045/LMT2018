0e81fbbecbc561c28608f3c38595a6558d894743
houshangling <houshangling@datangmobile.cn>
2018-09-20 08:54:34 +0800

    增加netplan项目

---
+++ Src/SCMT/NetPlan/DevMibCmdHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetPlan
+{
+	// 网规时查询设备属性对应的命令
+	public class DevMibCmdHelper
+	{
+		#region 公共方法
+
+		#endregion
+
+
+		#region 私有属性
+
+
+
+		#endregion
+	}
+}
