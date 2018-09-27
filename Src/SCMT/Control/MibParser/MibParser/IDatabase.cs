97e3a2280c095a9793e6c03f62f16a8886288ee9
mayi <mayi@datangmobile.cn>
2018-09-27 17:20:58 +0800

    SCMT-16 补充提交IDataBase.cs文件

--- Src/SCMT/Control/MibParser/MibParser/IDatabase.cs
+++ Src/SCMT/Control/MibParser/MibParser/IDatabase.cs
@@ -1,168 +1,188 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using MIBDataParser.JSONDataMgr;
 
 namespace MIBDataParser
 {
-    public interface IReDataByEnglishName
-    {
-        string oid { get; }
-        string mibSyntax { get; }//MIB_Syntax取值类型
-        string mangerValue { get; }//取值范围
-        string mibDesc { get; }//描述信息 MIBDesc
-        string parentOid { get; }//父oid，table=""
-    }
-    public interface IReDataByOid
-    {
-        string nameEn { get; }
-        string isLeaf { get; }
-        string indexNum { get; }
-    }
+	#region 定义json类
 
-    public interface IReDataByTableEnglishNameChild
-    {
-        string childNameMib { get;  } // "alarmCauseNostring ,
-        string childNo { get;  } // 1,
-        string childOid { get;  } // "1.1.1.1.1.1",
-        string childNameCh { get;  } // "告警原因编号",
-        string isMib { get;  } // 1,
-        string ASNType { get;  } // "Integer32",
-        string OMType { get;  } // "s32",
-        string UIType { get;  } // 0,
-        string managerValueRange { get;  } // "0-2147483647",
-        string defaultValue { get;  } // "×",
-        string detailDesc { get;  } // "告警原因编号， 取值  :0..2147483647。",
-        string leafProperty { get;  } // 0,
-        string unit { get; } // ""
+	//顶级MIB INFO类，保存所有的mib.json文件中信息
+	public class MibTree
+	{
+		public string mibVersion;
+		public int tableNum;
+		public List<MibTable> tableList;
 
-        bool idIndex { get; set; }		// TODO 为什么没有解析出这个属性？
-    }
-    public interface IReDataByTableEnglishName
-    {
-        string oid { get; }
-        string indexNum { get; }
-        List<IReDataByTableEnglishNameChild> childrenList { get; }
-    }
+		public MibTree()
+		{
+			tableList = new List<MibTable>();
+		}
+	}
 
-    public interface IReCmdDataByCmdEnglishName
-    {
-        string m_cmdNameEn { get; } // 命令的英文名
-        string m_tableName { get; } // 命令的mib表英文名
-        string m_cmdType { get;} //命令类型
-        string m_cmdDesc { get; } //命令描述
-        List<string> m_leaflist { get; } // 命令节点名
-    }
+	// mib table类定义
+	public class MibTable
+	{
+		public string nameMib;
+		public string oid;		// 表入口oid，不带前缀
+		public string nameCh;
+		public int indexNum;
+		public string mibSyntax;
+		public string mibDesc;
+		public List<MibLeaf> childList;
 
-    /// <summary>
-    /// 委托 : 初始化Database 结果
-    /// </summary>
-    /// <param name="result">初始化成功,true;失败,false</param>
-    public delegate void ResultInitData(bool result);
+		public MibTable()
+		{
+			childList = new List<MibLeaf>();
+		}
+	}
 
-    /// <summary>
-    /// 操作数据库接口 : 初始化, 查询. 没有修改的接口.
-    /// </summary>
-    public interface IDatabase
-    {
-        /// <summary>
-        /// 获取单实例数据库句柄
-        /// </summary>
-        /// <returns>Database</returns>
-        /// Database GetInstance();
+	// mib leaf 类定义
+	public class MibLeaf
+	{
+		public string childNameMib;
+		public int childNo;
+		public string childOid;
+		public string childNameCh;
+		public int isMib;
+		public string ASNType;
+		public string OMType;
+		public int UIType;
+		public string managerValueRange;
+		public string defaultValue;
+		public string detailDesc;
+		public int leafProperty;
+		public string unit;
+		public string IsIndex;	//取值：True False
+		public string mibSyntax;
+		public string mibDesc;
+		public bool IsTable = false;
 
-        /// <summary>
-        /// 初始化的线程 : 初始化(1.解压lm.dtz;2.解析.mdb;3.解析json;)
-        /// </summary>
-        /// <param name="connectIp">信息归属的标识</param>
-        void initDatabase(string connectIp);
+		public MibLeaf()
+		{
+			IsTable = false;
+		}
 
-        /// <summary>
-        /// 删除断链的ip的数据库
-        /// </summary>
-        /// <param name="connectIp">信息归属的标识</param>
-        /// <returns></returns>
-        bool delDatabase(string connectIp);
+		// 传入mibtable，构造一个leaf对象，目的是便于使用名称进行搜索
+		public MibLeaf(MibTable mt)
+		{
+			childNameCh = mt.nameCh;
+			childOid = mt.oid;
+			childNameMib = mt.nameMib;
+			mibSyntax = mt.mibSyntax;
+			mibDesc = mt.mibDesc;
+			IsTable = true;
+		}
+	}
 
-        /// <summary>
-        /// 返回初始化 initDatabase 的结果。void ResultInitData(bool result);
-        /// </summary>
-        /// <param name="result">true: 初始化成功； flase: 初始化失败。</param>
-        /// <returns>void</returns>
-        /// ResultInitData resultInitData;
+	#endregion
 
-        ///查询数据
-        /// <summary>
-        /// [查询]通过节点英文名字，查询节点信息。支持多节点查找。
-        /// </summary>
-        /// <param name="reData">其中key为查询的英文名(需要输入)，value为对应的节点信息。</param>
-        /// <param name="connectIp">信息归属的标识</param>
-        /// <param name="err">查询失败的原因</param>
-        /// <returns></returns>
-        bool getDataByEnglishName(Dictionary<string, IReDataByEnglishName> reData, string connectIp, out string err);
-        /// <summary>
-        /// [查询]通过节点英文名字，查询节点信息。支持单节点查找。
-        /// </summary>
-        /// <param name="nameEn">节点英文名字</param>
-        /// <param name="reData">节点信息</param>
-        /// <param name="curConnectIp">信息归属的标识</param>
-        /// <returns></returns>
-        bool getDataByEnglishName(string nameEn, out IReDataByEnglishName reData, string curConnectIp, out string err);
+	/// <summary>
+	/// 委托 : 初始化Database 结果
+	/// </summary>
+	/// <param name=result>初始化成功,true;失败,false</param>
+	public delegate void ResultInitData(bool result);
 
-        /// <summary>
-        /// [查询]通过节点oid，查询节点信息。支持多节点查找。
-        /// </summary>
-        /// <param name="reData">其中key为查询的oid(需要输入)，value为对应的节点信息。</param>
-        /// <param name="connectIp">信息归属的标识</param>
-        /// <param name="err">查询失败的原因</param>
-        /// <returns></returns>
-        //bool getDataByOid(string oid, string connectIp, out IReDataByOid reData, out string err);
-        bool getDataByOid(Dictionary<string, IReDataByOid>reData, string connectIp, out string err);
+	/// <summary>
+	/// 操作数据库接口 : 初始化, 查询. 没有修改的接口.
+	/// </summary>
+	public interface IDatabase
+	{
+		/// <summary>
+		/// 获取单实例数据库句柄
+		/// </summary>
+		/// <returns>Database</returns>
+		/// Database GetInstance();
 
-        /// <summary>
-        /// [查询]通过表的英文名，查询表的信息。支持多表查找。
-        /// </summary>
-        /// <param name="reData">其中key为查询的表名(需要输入)，value为对应的表的信息。</param>
-        /// <param name="connectIp">信息归属的标识</param>
-        /// <param name="err">查询失败的原因</param>
-        /// <returns></returns>
-        bool getDataByTableEnglishName(Dictionary<string, IReDataByTableEnglishName> reData, string connectIp, out string err);
+		/// <summary>
+		/// 初始化的线程 : 初始化(1.解压lm.dtz;2.解析.mdb;3.解析json;)
+		/// </summary>
+		/// <param name=connectIp>信息归属的标识</param>
+		void initDatabase(string connectIp);
 
-        /// <summary>
-        /// [查询]通过命令的英文名，查询命令的信息。支持多个命令查找。
-        /// </summary>
-        /// <param name="reData">其中key为查询的命令英文名(需要输入)，value为对应的命令的信息。</param>
-        /// <param name="connectIp">信息归属的标识</param>
-        /// <param name="err">查询失败的原因</param>
-        /// <returns></returns>
-        bool getCmdDataByCmdEnglishName(Dictionary<string, IReCmdDataByCmdEnglishName> reData, string connectIp, out string err);
+		/// <summary>
+		/// 删除断链的ip的数据库
+		/// </summary>
+		/// <param name=connectIp>信息归属的标识</param>
+		/// <returns></returns>
+		bool delDatabase(string connectIp);
 
-        /// <summary>
-        /// 获取所有的trap 类型
-        /// Dictionary<string, Dictionary<string, string>> ; Dictionary<每个TrapOid_ENB5216, 其他信息Dictionary(TrapID,TrapOid,TrapTypeDes)>
-        /// Dictionary<string, string> : Dictionary({ TrapID:value},{TrapOid:value},{TrapTypeDes:value})
-        /// </summary>
-        /// <returns>10组trap</returns>
-        Dictionary<string, Dictionary<string, string>> getTrapInfo();
+		/// <summary>
+		/// 返回初始化 initDatabase 的结果。void ResultInitData(bool result);
+		/// </summary>
+		/// <param name=result>true: 初始化成功； flase: 初始化失败。</param>
+		/// <returns>void</returns>
+		/// ResultInitData resultInitData;
 
-        /* 为 cfg 操作模块提供相关接口 */
-        /// <summary>
-        /// 解压lmdtz文件到指定目录下
-        /// </summary>
-        /// <param name="strFileToUnzip">目标dtz文件</param>
-        /// <param name="strFileToDirectory">解压释放目录</param>
-        /// <param name="err"></param>
-        /// <returns></returns>
-        bool cfgUnzipDtz(string strFileToUnzip, string strFileToDirectory, out string err);
+		///查询数据
+		/// <summary>
+		/// [查询]通过节点英文名字，查询节点信息。支持多节点查找。
+		/// </summary>
+		/// <param name=reData>其中key为查询的英文名(需要输入)，value为对应的节点信息。</param>
+		/// <param name=connectIp>信息归属的标识</param>
+		/// <param name=err>查询失败的原因</param>
+		/// <returns></returns>
+		bool getDataByEnglishName(Dictionary<string, MibLeaf> reData, string connectIp, out string err);
 
-        /// <summary>
-        /// 通过 sql 语句获取 数据
-        /// </summary>
-        /// <param name="fileName"></param>
-        /// <param name="sqlContent"></param>
-        /// <returns></returns>
-        //Object cfgGetRecordByAccessDb(string fileName, string sqlContent);
-    }
+		/// <summary>
+		/// [查询]通过节点英文名字，查询节点信息。支持单节点查找。
+		/// </summary>
+		/// <param name=nameEn>节点英文名字</param>
+		/// <param name=reData>节点信息</param>
+		/// <param name=curConnectIp>信息归属的标识</param>
+		/// <returns></returns>
+		bool getDataByEnglishName(string nameEn, out MibLeaf reData, string curConnectIp, out string err);
+
+		/// <summary>
+		/// [查询]通过节点oid，查询节点信息。支持多节点查找。
+		/// </summary>
+		/// <param name=reData>其中key为查询的oid(需要输入)，value为对应的节点信息。</param>
+		/// <param name=connectIp>信息归属的标识</param>
+		/// <param name=err>查询失败的原因</param>
+		/// <returns></returns>
+		bool GetMibDataByOids(Dictionary<string, MibLeaf> reData, string connectIp, out string err);
+
+		/// <summary>
+		/// [查询]通过表的英文名，查询表的信息。支持多表查找。
+		/// </summary>
+		/// <param name=reData>其中key为查询的表名(需要输入)，value为对应的表的信息。</param>
+		/// <param name=connectIp>信息归属的标识</param>
+		/// <param name=err>查询失败的原因</param>
+		/// <returns></returns>
+		bool GetMibDataByTableName(Dictionary<string, MibTable> reData, string connectIp, out string err);
+
+		/// <summary>
+		/// [查询]通过命令的英文名，查询命令的信息。支持多个命令查找。
+		/// </summary>
+		/// <param name=reData>其中key为查询的命令英文名(需要输入)，value为对应的命令的信息。</param>
+		/// <param name=connectIp>信息归属的标识</param>
+		/// <param name=err>查询失败的原因</param>
+		/// <returns></returns>
+		bool GetCmdDataByNames(Dictionary<string, CmdMibInfo> reData, string connectIp, out string err);
+
+		/// <summary>
+		/// 获取所有的trap 类型
+		/// Dictionary<string, Dictionary<string, string>> ; Dictionary<每个TrapOid_ENB5216, 其他信息Dictionary(TrapID,TrapOid,TrapTypeDes)>
+		/// Dictionary<string, string> : Dictionary({ TrapID:value},{TrapOid:value},{TrapTypeDes:value})
+		/// </summary>
+		/// <returns>10组trap</returns>
+		Dictionary<string, Dictionary<string, string>> GetTrapInfo();
+
+		/* 为 cfg 操作模块提供相关接口 */
+		/// <summary>
+		/// 解压lmdtz文件到指定目录下
+		/// </summary>
+		/// <param name=strFileToUnzip>目标dtz文件</param>
+		/// <param name=strFileToDirectory>解压释放目录</param>
+		/// <param name=err></param>
+		/// <returns></returns>
+		bool cfgUnzipDtz(string strFileToUnzip, string strFileToDirectory, out string err);
+
+		/// <summary>
+		/// 通过 sql 语句获取 数据
+		/// </summary>
+		/// <param name=fileName></param>
+		/// <param name=sqlContent></param>
+		/// <returns></returns>
+		//Object cfgGetRecordByAccessDb(string fileName, string sqlContent);
+	}
 }
 
