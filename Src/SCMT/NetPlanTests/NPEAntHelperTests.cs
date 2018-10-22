67b0a15ebc1a2bf84f1133deaef14da9bcb4d25e
houshangling <houshangling@datangmobile.cn>
2018-10-22 14:00:01 +0800

    增加天线阵信息文件解析，增加单元测试用例

---
+++ Src/SCMT/NetPlanTests/NPEAntHelperTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetPlan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetPlan.Tests
+{
+	[TestClass()]
+	public class NPEAntHelperTests
+	{
+		[TestMethod()]
+		public void GetAllAntTypeInfoTest()
+		{
+			var ret = NPEAntHelper.GetInstance().GetAllAntTypeInfo();
+			Assert.IsNotNull(ret);
+		}
+	}
+}
