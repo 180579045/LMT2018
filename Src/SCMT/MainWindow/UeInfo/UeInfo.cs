8c8995def82f77b18179968fa427f144b3d52e82
zhuwentian <zhuwentian@datangmobile.cn>
2018-11-05 13:36:48 +0800

    UeINfo  UE

---
+++ Src/SCMT/MainWindow/UeInfo/UeInfo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCMTMainWindow.UeInfo
+{
+    public class UeInformation
+    {
+        private int num;
+        private string info1;
+
+        private string info2;
+
+        private string info3;
+        private ObservableCollection<ChildrenUeInfo> info4;
+        public int Num
+        {
+            get { return this.num; }
+            set { this.num = value; }
+        }
+
+        public string Info1
+        {
+            get
+            {
+                return info1;
+            }
+
+            set
+            {
+                info1 = value;
+            }
+        }
+
+        public string Info2
+        {
+            get
+            {
+                return info2;
+            }
+
+            set
+            {
+                info2 = value;
+            }
+        }
+
+        public string Info3
+        {
+            get
+            {
+                return info3;
+            }
+
+            set
+            {
+                info3 = value;
+            }
+        }
+
+        public ObservableCollection<ChildrenUeInfo> Info4
+        {
+            get
+            {
+                return info4;
+            }
+
+            set
+            {
+                info4 = value;
+            }
+        }
+    }
+}
---
+++ Src/SCMT/MainWindow/View/UeInfo.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SCMTMainWindow.View
+{
+    /// <summary>
+    /// UeInfo.xaml 的交互逻辑
+    /// </summary>
+    public partial class UeInfo : UserControl
+    {
+        public UeInfo()
+        {
+            InitializeComponent();
+        }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            EditColumns edclw = new EditColumns(this.dataGrid.Columns);
+            edclw.ShowDialog();
+            if (!edclw.IsOK)
+            {
+                return;
+            }
+            for (int k = 0; k < this.dataGrid.Columns.Count; k++)
+            {
+                this.dataGrid.Columns[k].Visibility = Visibility.Visible;
+            }
+            for (int i = 0; i < edclw.HiddenHeader1.Count; i++)
+            {
+                for (int j = 0; j < this.dataGrid.Columns.Count; j++)
+                {
+                    if (edclw.HiddenHeader1[i].DisplayIndex == this.dataGrid.Columns[j].DisplayIndex)
+                    {
+                        this.dataGrid.Columns[j].Visibility = Visibility.Hidden;
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
