b2d4ae0c30ac6ace0c86120e0f90ef6531108c7f
houshangling <houshangling@datangmobile.cn>
2018-11-06 19:35:04 +0800

    增加接口：清除连接信息

--- Src/SCMT/NetPlan/NetPlanLinkMgr.cs
+++ Src/SCMT/NetPlan/NetPlanLinkMgr.cs
@@ -13,6 +13,16 @@ namespace NetPlan
 	{
 		#region 公共接口
 
+		internal void Clear()
+		{
+			lock (m_syncObj)
+			{
+				m_listWaitPrevRru.Clear();
+				m_mapLinks.Clear();
+				m_listParsedRru.Clear();
+			}
+		}
+
 		/// <summary>
 		/// 从MIB信息中解析出所有的连接信息，保存在本地
 		/// </summary>
@@ -20,6 +30,9 @@ namespace NetPlan
 		/// <returns></returns>
 		internal bool ParseLinksFromMibInfo(MAP_DEVTYPE_DEVATTRI mapMibInfo)
 		{
+			// 解析新的信息，删除旧的信息
+			Clear();
+
 			if (null == mapMibInfo || 0 == mapMibInfo.Count)
 			{
 				Log.Error("传入MIB信息为空");
@@ -46,7 +59,7 @@ namespace NetPlan
 				return false;
 			}
 
-			// 解析board与rru或rhub之间的连接
+			// 解析board与rru之间的连接
 			if (!ParseBoardToRruLink(rruList, boardList))
 			{
 				Log.Error("解析board与rru之间的连接信息失败");
@@ -79,6 +92,28 @@ namespace NetPlan
 				return false;
 			}
 
+			// 解析board与rhub之间的连接
+			if (!mapMibInfo.ContainsKey(EnumDevType.rhub))
+			{
+				Log.Debug("未找到RHUB设备类型信息");
+				return true;
+			}
+
+			var rhubList = mapMibInfo[EnumDevType.rhub];
+			if (rhubList.Count == 0)
+			{
+				Log.Debug("未找到RHUB设备类型信息");
+				return true;
+			}
+
+			if (!ParseBoardToRhubLink(rhubList, boardList))
+			{
+				
+			}
+
+			// 解析rhub与pico之间的连接
+
+
 			return true;
 		}
 
@@ -88,7 +123,10 @@ namespace NetPlan
 		/// <returns></returns>
 		internal Dictionary<EnumDevType, List<WholeLink>> GetAllLink()
 		{
-			return m_mapLinks;
+			lock (m_syncObj)
+			{
+				return m_mapLinks;
+			}
 		}
 
 		internal NetPlanLinkMgr()
@@ -96,6 +134,35 @@ namespace NetPlan
 			m_mapLinks = new Dictionary<EnumDevType, List<WholeLink>>();
 			m_listParsedRru = new List<ParsedRruInfo>();
 			m_listWaitPrevRru = new List<ParsedRruInfo>();
+		}
+
+		/// <summary>
+		/// 增加连接到列表中
+		/// </summary>
+		/// <param name="srcEndpoint"></param>
+		/// <param name="dstEndpoint"></param>
+		/// <param name="linkType"></param>
+		internal void AddLinkToList(LinkEndpoint srcEndpoint, LinkEndpoint dstEndpoint, EnumDevType linkType)
+		{
+			var link = new WholeLink
+			{
+				m_srcEndPoint = srcEndpoint,
+				m_dstEndPoint = dstEndpoint,
+				m_linkType = linkType
+			};
+
+			lock (m_syncObj)
+			{
+				if (m_mapLinks.ContainsKey(linkType))
+				{
+					m_mapLinks[linkType].Add(link);
+				}
+				else
+				{
+					var listLink = new List<WholeLink> { link };
+					m_mapLinks[linkType] = listLink;
+				}
+			}
 		}
 
 		#endregion
@@ -157,6 +224,37 @@ namespace NetPlan
 
 			AddLinkToList(boardEndpoint, rruEndPoint, EnumDevType.board_rru);
 
+			return true;
+		}
+
+		/// <summary>
+		/// 生成一条board到rhub设备的连接
+		/// </summary>
+		/// <param name="strBoardIndex"></param>
+		/// <param name="nBoardIrPort"></param>
+		/// <param name="strRhubIndex"></param>
+		/// <param name="nRhubIrPort"></param>
+		/// <returns></returns>
+		private bool GenerateBoardToRhubLink(string strBoardIndex, int nBoardIrPort, string strRhubIndex, int nRhubIrPort)
+		{
+			var boardEndpoint = new LinkEndpoint
+			{
+				devType = EnumDevType.board,
+				strDevIndex = strBoardIndex,
+				nPortNo = nBoardIrPort,
+				portType = EnumPortType.bbu_to_rhub
+			};
+
+			var rruEndPoint = new LinkEndpoint
+			{
+				devType = EnumDevType.rhub,
+				strDevIndex = strRhubIndex,
+				nPortNo = nRhubIrPort,
+				portType = EnumPortType.rhub_to_bbu
+			};
+
+			// todo 此处board到rhub设备的连接类型设置为board_rru，校验功能是否会检查
+			AddLinkToList(boardEndpoint, rruEndPoint, EnumDevType.board_rru);
 			return true;
 		}
 
@@ -234,29 +332,14 @@ namespace NetPlan
 		}
 
 		/// <summary>
-		/// 增加连接到列表中
+		/// 生成一条rhub到rhub的连接
 		/// </summary>
-		/// <param name="srcEndpoint"></param>
-		/// <param name="dstEndpoint"></param>
-		/// <param name="linkType"></param>
-		private void AddLinkToList(LinkEndpoint srcEndpoint, LinkEndpoint dstEndpoint, EnumDevType linkType)
+		/// <param name="prevRhubInfo"></param>
+		/// <param name="curRhubInfo"></param>
+		/// <returns></returns>
+		private bool GenerateRhubToRhubLink(ParsedRruInfo prevRhubInfo, ParsedRruInfo curRhubInfo)
 		{
-			var link = new WholeLink
-			{
-				m_srcEndPoint = srcEndpoint,
-				m_dstEndPoint = dstEndpoint,
-				m_linkType = linkType
-			};
-
-			if (m_mapLinks.ContainsKey(linkType))
-			{
-				m_mapLinks[linkType].Add(link);
-			}
-			else
-			{
-				var listLink = new List<WholeLink> { link };
-				m_mapLinks[linkType] = listLink;
-			}
+			throw new NotImplementedException("rhub到rhub的级联模式尚未支持");
 		}
 
 		// 处理没有找到上级rru的信息
@@ -264,34 +347,42 @@ namespace NetPlan
 		{
 			var listDel = new List<ParsedRruInfo>();
 			// 处理未找到上级rru信息的rru。
-			while (m_listWaitPrevRru.Count > 0)
+			lock (m_syncObj)
 			{
-				listDel.Clear();
-				for (int i = m_listWaitPrevRru.Count - 1; i >= 0; i--)
+				while (m_listWaitPrevRru.Count > 0)
 				{
-					var waitPrevRru = m_listWaitPrevRru.ElementAt(i);
-					foreach (var existedRru in m_listParsedRru)
+					listDel.Clear();
+					for (int i = m_listWaitPrevRru.Count - 1; i >= 0; i--)
 					{
-						if (!waitPrevRru.IsPrevRru(existedRru)) continue;
-
-						if (!GenerateRruToRruLink(existedRru, waitPrevRru))
+						var waitPrevRru = m_listWaitPrevRru.ElementAt(i);
+						foreach (var existedRru in m_listParsedRru)
 						{
-							Log.Error("");
-							return false;
+							if (!waitPrevRru.IsPrevRru(existedRru)) continue;
+
+							if (!GenerateRruToRruLink(existedRru, waitPrevRru))
+							{
+								Log.Error("");
+								return false;
+							}
 						}
+
+						// 把waitPrevRru加入到m_listParsedRru队列
+						AddElementToParsedRruList(waitPrevRru);
+						listDel.Add(waitPrevRru);
 					}
 
-					// 把waitPrevRru加入到m_listParsedRru队列
-					m_listParsedRru.Add(waitPrevRru);
-					listDel.Add(waitPrevRru);
-				}
-
-				foreach (var item in listDel)
-				{
-					m_listWaitPrevRru.Remove(item);
+					foreach (var item in listDel)
+					{
+						m_listWaitPrevRru.Remove(item);
+					}
 				}
 			}
 			return true;
+		}
+
+		private bool HandleWaitPrevRhubList()
+		{
+			throw new NotImplementedException();
 		}
 
 		/// <summary>
@@ -306,10 +397,13 @@ namespace NetPlan
 				return null;
 			}
 
-			return m_listParsedRru.FirstOrDefault(curRruInfo.IsPrevRru);
+			lock (m_syncObj)
+			{
+				return m_listParsedRru.FirstOrDefault(curRruInfo.IsPrevRru);
+			}
 		}
 
-		// 处理rru光口连接的信息
+		// 处理rru光口连接的信息。todo 应该能从board_rru中读到ir信息
 		private bool HandleRruOfpLinkInfo(int nOfpIndex, DevAttributeInfo rru, List<DevAttributeInfo> boardDevList)
 		{
 			// 取出rru编号、接入板卡的信息
@@ -395,14 +489,14 @@ namespace NetPlan
 					return false;
 				}
 
-				if ("2" == workMode) // todo 负荷分担模式默认可以级联？
+				if ("2" == workMode || "5" == workMode)
 				{
-					m_listParsedRru.Add(parsedRru);
+					AddElementToParsedRruList(parsedRru);
 				}
 				return true;
 			}
 
-			if ("2" != workMode && "4" != workMode) // todo 负荷分担模式，是否允许级联？
+			if ("2" != workMode && "5" != workMode)
 			{
 				Log.Error($"索引为{rruIndex}的RRU工作模式为{workMode}，光口级联级数错误");
 				return false;
@@ -413,7 +507,7 @@ namespace NetPlan
 			if (null == prevRru)
 			{
 				// 没有找到上级rru，放入等待队列
-				m_listWaitPrevRru.Add(parsedRru);
+				AddElementToWaitPrevList(parsedRru);
 				return true;
 			}
 
@@ -423,7 +517,8 @@ namespace NetPlan
 				return false;
 			}
 
-			m_listParsedRru.Add(parsedRru);
+			AddElementToParsedRruList(parsedRru);
+
 			return true;
 		}
 
@@ -505,6 +600,208 @@ namespace NetPlan
 			return true;		// 如果中间有break调用，这里就会返回false
 		}
 
+		/// <summary>
+		/// 解析板卡到rhub之间的连接
+		/// netIROptPlanEntry
+		/// </summary>
+		/// <param name="rhubList"></param>
+		/// <param name="boardList"></param>
+		/// <returns></returns>
+		private bool ParseBoardToRhubLink(IReadOnlyCollection<DevAttributeInfo> rhubList,
+			IReadOnlyCollection<DevAttributeInfo> boardList)
+		{
+			// rhub设备可能是4个光口连接，也可能是2个光口连接
+			foreach (var rhubDai in rhubList)
+			{
+				// 取出rhub设备，读取连接板卡的架框槽
+				if (null == rhubDai)
+				{
+					continue;
+				}
+
+				for (var i = 1; i < 5; i++)
+				{
+					if (!HandleRhubOfpLinkInfo(i, rhubDai, boardList))
+					{
+						return false;
+					}
+				}
+			}
+
+			// 5G的pico设备只有一个网口，通过网口与rhub的电口相连
+			// 在mib中，netRRUOfp1AccessEthernetPort表示连接的是对端Hub eth口
+			return HandleWaitPrevRhubList();
+		}
+
+		/// <summary>
+		/// 处理rhub设备的光口号信息
+		/// </summary>
+		/// <param name="nOfpIndex">光口索引，rhub2.0才有4个光口</param>
+		/// <param name="rhub">rhub设备属性信息</param>
+		/// <param name="boardDevList">板卡信息列表</param>
+		/// <returns></returns>
+		private bool HandleRhubOfpLinkInfo(int nOfpIndex, DevAttributeInfo rhub, IEnumerable<DevAttributeInfo> boardDevList)
+		{
+			// 取出rru编号、接入板卡的信息
+			var rhubNo = rhub.GetFieldOriginValue("netRHUBNo");
+
+			// 级联模式的rru，连接的板卡的信息和第1级rru连接板卡信息一样
+			var rackNo = rhub.GetFieldOriginValue("netRHUBAccessRackNo");
+			var shelfNo = rhub.GetFieldOriginValue("netRHUBAccessShelfNo");
+			var boardTypeInRhub = rhub.GetFieldOriginValue("netRHUBAccessBoardType");
+			var hubIndex = $".{rhubNo}";
+
+			// 级联模式才会有接入级数>1的情况
+			var workMode = rhub.GetFieldOriginValue("netRHUBOfpWorkMode");
+
+			var slotNoMib = "netRHUBAccessSlotNo";		// 光口接入的槽位号
+			if (nOfpIndex > 1)
+			{
+				slotNoMib = $"netRHUBOfp{nOfpIndex}SlotNo";
+			}
+
+			// 获取光口n连接的槽位号
+			var slotNo = rhub.GetFieldOriginValue(slotNoMib);
+			if (null == slotNo)
+			{
+				Log.Debug($"当前版本MIB中不包含{slotNoMib}字段");
+				return true;
+			}
+
+			if ("-1" == slotNo)
+			{
+				Log.Debug($"索引为{hubIndex}的RHUB光口{nOfpIndex}未连接设备");
+				return true;
+			}
+
+			var boardIndex = $".{rackNo}.{shelfNo}.{slotNo}";
+
+			// 根据索引获取板卡信息
+			var boardInfo = boardDevList.FirstOrDefault(item => item.m_strOidIndex == boardIndex);
+			if (null == boardInfo)
+			{
+				Log.Error($"索引为{hubIndex}的RHUB连接板卡索引{boardIndex}，未找到对应的板卡");
+				return false;
+			}
+
+			// 判断板卡的类型是否相同
+			var boardTypeInBoard = boardInfo.GetFieldOriginValue("netBoardType");
+			if (boardTypeInBoard != boardTypeInRhub)
+			{
+				Log.Error($"索引为{hubIndex}的RHUB连接板卡类型{boardTypeInRhub}，而相同索引{boardIndex}的板卡类型为{boardTypeInBoard}，二者不一致");
+				return false;
+			}
+
+			// 获取每个光口连接的对端光口号和接入级数
+			var ofpPortMibName = $"netRHUBOfp{nOfpIndex}AccessOfpPortNo";
+			var accessPosMibName = $"netRHUBOfp{nOfpIndex}AccessLinePosition";
+
+			var remoteOfPort = rhub.GetFieldOriginValue(ofpPortMibName);
+			if (null == remoteOfPort)
+			{
+				Log.Debug($"当前版本MIB中不包含{ofpPortMibName}字段");
+				return true;
+			}
+
+			if ("-1" == remoteOfPort)
+			{
+				Log.Debug($"索引为{hubIndex}的RHUB光口{nOfpIndex}未连接任何设备");
+				return true;
+			}
+
+			var accessPos = rhub.GetFieldOriginValue(accessPosMibName);
+			if (null == accessPos)
+			{
+				Log.Debug($"当前版本MIB中不包含{accessPosMibName}字段");
+				return true;
+			}
+
+			if ("-1" == accessPos) // todo 需要注意非级联模式设置为默认值-1的情况
+			{
+				Log.Error($"索引为{hubIndex}的RRU光口{nOfpIndex}连接对端光口{remoteOfPort}，但接入级数为-1，无效信息");
+				return false;
+			}
+
+			var parsedRhub = new ParsedRruInfo
+			{
+				nBoardIrPort = int.Parse(remoteOfPort),
+				nCurLinePos = int.Parse(accessPos),
+				nRruIrPort = nOfpIndex,
+				strBoardIndex = boardIndex,
+				strRruIndex = hubIndex
+			};
+
+			// rhub连接板卡时，接入级数为1；正常模式，接入级数为1
+			if ("1" == accessPos)
+			{
+				// todo 在netIROptPlanEntry中查找是否存在这条连接。2级级联的设备不存在于这张表中
+
+				if (!GenerateBoardToRhubLink(boardIndex, int.Parse(remoteOfPort), hubIndex, nOfpIndex))
+				{
+					Log.Error($"生成板卡到rhub{hubIndex}的连接失败");
+					return false;
+				}
+
+				if ("2" == workMode || "5" == workMode)		// 级联；负荷分担 + 级联
+				{
+					AddElementToParsedRruList(parsedRhub);
+				}
+				return true;
+			}
+
+			if ("2" != workMode && "5" != workMode)		// 级联；负荷分担+级联
+			{
+				Log.Error($"索引为{hubIndex}的RHUB工作模式为{workMode}，光口级联级数错误");
+				return false;
+			}
+
+			// 接入级数大于1，则要找接入级数小1级的设备组成连接信息
+			var prevRhub = GetPrevRruInfo(parsedRhub);
+			if (null == prevRhub)
+			{
+				// 没有找到上级rru，放入等待队列
+				AddElementToWaitPrevList(parsedRhub);
+				return true;
+			}
+
+			if (!GenerateRhubToRhubLink(prevRhub, parsedRhub))
+			{
+				Log.Error("生成rru到rru的连接失败");
+				return false;
+			}
+
+			AddElementToParsedRruList(parsedRhub);      // 解析完成的就加入到列表中
+
+			return true;
+		}
+
+
+		private void AddElementToParsedRruList(ParsedRruInfo element)
+		{
+			if (null == element)
+			{
+				return;
+			}
+
+			lock (m_syncObj)
+			{
+				m_listParsedRru.Add(element);
+			}
+		}
+
+		private void AddElementToWaitPrevList(ParsedRruInfo element)
+		{
+			if (null == element)
+			{
+				return;
+			}
+
+			lock (m_syncObj)
+			{
+				m_listWaitPrevRru.Add(element);
+			}
+		}
+
 		#endregion
 
 
@@ -515,6 +812,8 @@ namespace NetPlan
 		private List<ParsedRruInfo> m_listParsedRru;		// 所有的工作模式为级联的rru信息
 
 		private List<ParsedRruInfo> m_listWaitPrevRru;		// 等待上一级的rru
+
+		private readonly object m_syncObj = new object();
 
 		#endregion
 	}
