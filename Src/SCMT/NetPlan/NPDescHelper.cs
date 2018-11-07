df0473299921ecd477888901adc437ea62071ad1
houshangling <houshangling@datangmobile.cn>
2018-11-07 09:33:37 +0800

    增加获取指定端口、设备、连接描述信息功能

---
+++ Src/SCMT/NetPlan/NPDescHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonUtility;
+
+namespace NetPlan
+{
+	/// <summary>
+	/// 网规模块各设备、端口描述信息助手
+	/// </summary>
+	public static class NPDescHelper
+	{
+
+		#region 公共接口区
+
+		public static string GetPortDesc()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+	}
+}
